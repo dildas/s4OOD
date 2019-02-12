7a396da4611ad55d5b8b11b27cbe79d6774cfc9c
dildas <47568855+dildas@users.noreply.github.com>
2019-02-12 21:35:59 +0530

    26.Dildas

---
+++ letter.java
@@ -0,0 +1,41 @@
+class letter 
+{
+public static void main(String args[])
+{
+
+int i,j=0;
+
+for(i=0;i<2;i++)
+	{
+		for(j=0;j<20;j++)
+		{
+			System.out.print("1");
+		}
+		System.out.println("");
+	}
+
+for(i=0;i<10;i++)
+	{
+	for(j=0;j<4;j++)
+		{			
+			System.out.print("1");
+		}
+	System.out.println("");
+	
+	}
+
+for(i=0;i<2;i++)
+	{
+		for(j=0;j<20;j++)
+		{
+			System.out.print("1");
+		}
+		System.out.println("");
+	}
+
+
+
+
+}
+}
+
